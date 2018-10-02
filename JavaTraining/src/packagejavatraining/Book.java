f1f592d4f73d0491c2866a97dcfd1dd50c454552
vikassaini3012 <43619825+vikassaini3012@users.noreply.github.com>
2018-10-02 15:13:19 +0530

    updatebook.java
    Run time  changes on GIT

--- JavaTraining/src/packagejavatraining/Book.java
+++ JavaTraining/src/packagejavatraining/Book.java
@@ -6,13 +6,13 @@ public class Book
 	public static void main(String args[]) {
 		int iresult, irem;
 		double dresult, drem;
-		iresult = 10 / 3;
-		irem = 10 % 3;
-		dresult = 10.0 / 3.0;
-		drem = 10.0 % 3.0;
-		System.out.println("Result and remainder of 10 / 3: " +
+		iresult = 10 / 4;
+		irem = 10 % 4;
+		dresult = 10.0 / 4.0;
+		drem = 10.0 % 4.0;
+		System.out.println("Result and remainder of 10 / 4: " +
 		iresult + " " + irem);
-		System.out.println("Result and remainder of 10.0 / 3.0: "
+		System.out.println("Result and remainder of 10.0 / 4.0: "
 		+ dresult + " " + drem);
 		}
 	
